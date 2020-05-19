90713fdebdfa104b610dd0384bb55d68b42e89b8
galgros <galgros@yahoo.fr>
2020-05-19 17:02:16 +0200

    05 symfony routing avancé

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug<[\da-z-]+>}", name="show")
+     */
+    public function show(string $slug = "Aucune série sélectionnée, veuillez choisir une série") :Response
+    {
+        return $this->render('wild/show.html.twig', [
+            'slug' => ucwords(str_replace('-', ' ', $slug))
         ]);
     }
 }
